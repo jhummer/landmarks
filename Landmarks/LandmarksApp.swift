b33c695b878dc6c1519dd53352f9106b8b441646
Joakim Hummer <joakim.hummer@gmail.com>
2023-01-25 15:48:09 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Joakim Hummer on 2023-01-25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
